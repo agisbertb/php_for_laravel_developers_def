bc1d3720b9826a2416acaeeda0f5952e6883afd7
agisbertb <agisbert@iesebre.com>
2023-02-27 18:49:34 +0100

    Refactoritzar la vista tasks per utilitzar models Laravel (Laravel Eloquent)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\TaskP;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,31 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 // HTTP -> REQUEST METHOD
 Route::get('/', function () {
-    $task1 = new stdClass();
-    $task1->id = 1;
-    $task1->title = "Task 1";
-    $task1->description = "Bla Bla Bla";
-    $task1->completed = 1;
-    $task2 = new stdClass();
-    $task2->id = 2;
-    $task2->title = "Task 2";
-    $task2->description = "Bla Bla Bla";
-    $task2->completed = 1;
-
-    $task3 = new stdClass();
-    $task3->id = 3;
-    $task3->title = "Task 3";
-    $task3->description = "Bla Bla Bla";
-    $task3->completed = 0;
-
-    $tasks = [
-        $task1,
-        $task2,
-        $task3
-    ];
-
     return view('tasks',[
-        'tasks'=> $tasks
+        'tasks'=> TaskP::all()
     ]);
 });
 
